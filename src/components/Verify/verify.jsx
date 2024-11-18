6251b6f7c507f713c1518de57288853f6cfdee5f
Omat-hub <141182163+Omat-hub@users.noreply.github.com>
2024-11-18 14:51:01 +0100

    how slider

--- src/components/Verify/verify.jsx
+++ src/components/Verify/verify.jsx
@@ -2,19 +2,21 @@ import "./Verify.css";
 
 const Verify = () => {
   return (
-    <section className="verify-main-container container-margin">
-      <div className="verify-left">
-        <p>Start Verifying Today</p>
-        <h2>Property Verification Made Simple.</h2>
-        <p className="text">
-          Get the clarity and confidence you need to make smart property
-          investments. Don’t wait until it’s too verify your property with
-          Verizone now.
-        </p>
-        <button type="button">Register</button>
-      </div>
+    <section className="verify-top-container container-margin">
+      <div className="verify-main-container">
+        <div className="verify-left">
+          <p>Start Verifying Today</p>
+          <h2>Property Verification Made Simple.</h2>
+          <p className="text">
+            Get the clarity and confidence you need to make smart property
+            investments. Don’t wait until it’s too verify your property with
+            Verizone now.
+          </p>
+          <button type="button">Register</button>
+        </div>
 
-      <div className="verify-image"></div>
+        <div className="verify-image"></div>
+      </div>
     </section>
   );
 };
